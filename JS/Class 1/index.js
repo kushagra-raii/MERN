17b4472a48d7c64626fc9ad785a3956f1a1d4022
kushagra-raii <kushagrarai83@gmail.com>
2023-03-15 21:36:19 +0530

    js started

---
+++ JS/Class 1/index.js
@@ -0,0 +1,18 @@
+console.log("Namastey Dunia");
+
+
+var a = 5 ;
+console.log(a);
+var a = 17;
+console.log(a);
+// the above thing cant happen in let 
+
+
+
+// Dynamic Typing - diff datatype 
+let lastName = "rai";
+console.log(lastName);
+lastName = 7;
+console.log(lastName);
+
+
