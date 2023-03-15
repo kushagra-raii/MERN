804e4368652f7939776db2343a9fb2ba5d889b3a
kushagra-raii <kushagrarai83@gmail.com>
2023-03-15 23:18:02 +0530

    JS 1 done

--- JS/Class 1/index.js
+++ JS/Class 1/index.js
@@ -1,18 +1,71 @@
-console.log("Namastey Dunia");
+// console.log("Namastey Dunia");
 
 
-var a = 5 ;
-console.log(a);
-var a = 17;
-console.log(a);
-// the above thing cant happen in let 
+// var a = 5 ;
+// console.log(a);
+// var a = 17;
+// console.log(a);
+// // the above thing cant happen in let 
 
 
 
-// Dynamic Typing - diff datatype 
-let lastName = "rai";
-console.log(lastName);
-lastName = 7;
-console.log(lastName);
+// // Dynamic Typing - diff datatype 
+// let lastName = "rai";
+// console.log(lastName);
+// lastName = 7;
+// console.log(lastName);
 
 
+// condition
+// let age = 19;
+// let stat = (age>18)? 'a' :'b' ;
+// console.log(stat);
+
+// let marks = 12;
+
+// if(marks >= 90) {
+//     console.log('A');
+// }
+// else if(marks >= 80) {
+//     console.log('B');
+// }
+// else if(marks >=70) {
+//     console.log('C');
+// }
+// else if(marks >= 60) {
+//     console.log('D');
+// }
+// else {
+//     console.log('E');
+//}
+
+// let num = 1;
+
+// switch(num) {
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3: console.log('C');
+//     break;
+//     default: console.log('D');
+// }
+
+
+// for(let i = 1; i<=5; i=i+1) {
+//     console.log("Babbar" + i);
+// }
+
+// let x = 1;
+
+// while(x <= 5) {
+//     console.log(x);
+//     x++;
+// }
+
+let y = 1;
+
+do {
+    console.log(y);
+    y++;
+} while(y < 10);
