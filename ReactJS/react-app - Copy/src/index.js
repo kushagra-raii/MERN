839b701ab1c08057d10f8be781eb7f19cddf1114
kushagra-raii <kushagrarai83@gmail.com>
2023-07-20 23:57:18 +0530

    RandomGif project started

--- ReactJS/react-app - Copy/src/index.js
+++ ReactJS/react-app - Copy/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./RouterApp/App";
+import App from "./RandomGif/App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
@@ -9,9 +9,9 @@ import "react-toastify/dist/ReactToastify.css"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div id="indexPage"> 
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
     <App  />
-    <ToastContainer/>
-    </BrowserRouter>
+    {/* <ToastContainer/> */}
+    {/* </BrowserRouter> */}
   </div>
 );
